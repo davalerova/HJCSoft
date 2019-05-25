6a77084805cb01001c2847fe3e1cdb04fdf204a7
davalerova <davalero@ieee.org>
2019-05-25 18:25:29 -0500

    Se organiza el proyecto en MVC

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("../vista/sample.fxml"));
+        primaryStage.setTitle("HJCSoft");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
